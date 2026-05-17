ae850f27523e5b376a249fc2a17035b1e047c26a
dimaswisodewo <mdimaswisodewo26@cjkt.idserve.net>
2026-05-17 20:23:26 +0700

    Initial Commit

---
+++ Slipi/SlipiApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlipiApp.swift
+//  Slipi
+//
+//  Created by Meynabel Dimas Wisodewo on 17/05/26.
+//
+
+import SwiftUI
+
+@main
+struct SlipiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
