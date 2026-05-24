cc06e182ce2fdd952d447c7f58162dac16cab407
syukronJazila <jazilasyukron12@gmail.com>
2026-05-24 19:05:58 +0700

    refactor: use dynamic SF Symbols for CardMusic icons

---
+++ Slipi/Views/CircleIcon.swift
@@ -0,0 +1,18 @@
+//
+//  CircleIcon.swift
+//  Slipi
+//
+//  Created by Muhammad Syukron Jazila on 24/05/26.
+//
+
+import SwiftUI
+
+struct CircleIcon: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircleIcon()
+}
